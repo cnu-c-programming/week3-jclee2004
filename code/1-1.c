2ccabadff356957097888a6ce96c035b13536880
JeongChan Lee <110677779+jclee2004@users.noreply.github.com>
2026-03-20 11:05:36 +0900

    Refactor main function to perform arithmetic operations
    Updated main function to include basic arithmetic operations.

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main() {
-
+int main(void) {
+    int a=10,b=5;
+    printf("%d + %d = %d\n",a,b,a+b);
+    printf("%d - %d = %d\n",a,b,a-b);
+    printf("%d * %d = %d\n",a,b,a*b);
+    printf("%d / %d = %d\n",a,b,a/b);
     return 0;
 }
