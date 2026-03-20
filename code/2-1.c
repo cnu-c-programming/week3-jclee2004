ff1efedb13522c2554f8a18e44af5d71e7c2fde0
JeongChan Lee <110677779+jclee2004@users.noreply.github.com>
2026-03-20 11:17:43 +0900

    Add increment function to modify and print value
    Added an increment function that modifies its parameter and prints the value.

--- code/2-1.c
+++ code/2-1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
+void increment(int x) {
+    ++x;
+    printf("%d\n",x);
+}
 
+int main() {
+    int x=10;
+
+    printf("%d\n",x);
+    increment(x);
+    printf("%d\n",x);
     return 0;
 }
