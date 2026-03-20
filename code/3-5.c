84483c0f387ab0107a7db7b49668288b6e7495d5
JeongChan Lee <110677779+jclee2004@users.noreply.github.com>
2026-03-20 11:47:53 +0900

    Implement variadic function for multiple data types
    Added a variadic function 'my_sum' to print strings, characters, or sum integers.

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void my_sum(char c,int count,...) {
+    va_list ap;
+    va_start(ap,count);
+    switch (c) 
+    {
+    case 'S':
+        for (int i=0;i<count;i++) {
+            printf("%s ",va_arg(ap,char *));
+        }
+        break;
+    case 'C':
+        for (int i=0;i<count;i++) {
+            printf("%c",va_arg(ap,int));
+        }
+        break;
+    case 'D':
+        int total=0;
+        for (int i=0;i<count;i++) {
+            total += va_arg(ap,int);
+        }
+        printf("%d",total);
+        break;
+    }
+    printf("\n");
+    va_end(ap);
+    return;
+}
+
 int main() {
+    my_sum('S',2,"Hello","World");
+    my_sum('C',3,'C','N','U');
+    my_sum('D',4,10,20,30,40);
 
     return 0;
 }
